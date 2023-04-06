2e183066a40b4db25d3f5143c16a661973544d1e
Haxxproxx202 <pjanecki88@gmail.com>
2023-04-06 19:35:22 +0200

    Change and reset password with email confirmation with django.contrib.auth.views

--- backend/account/views.py
+++ backend/account/views.py
@@ -5,24 +5,24 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
 
-def user_login(request):
-    if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd['username'],
-                                password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authentication has beem successful.')
-                else:
-                    return HttpResponse('Account is not active.')
-            else:
-                return HttpResponse('Invalid authentication data.')
-    else:
-        form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
+# def user_login(request):
+#     if request.method == "POST":
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(username=cd['username'],
+#                                 password=cd['password'])
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     return HttpResponse('Authentication has beem successful.')
+#                 else:
+#                     return HttpResponse('Account is not active.')
+#             else:
+#                 return HttpResponse('Invalid authentication data.')
+#     else:
+#         form = LoginForm()
+#     return render(request, 'account/login.html', {'form': form})
 
 
 @login_required
